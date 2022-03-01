622d1d4e62944fa3904ab45099c19afd50401425
Danila <68434397+koman4git@users.noreply.github.com>
2022-03-01 12:45:22 +0300

    исправление ошибок в ДЗ 13

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-
-
-
 let toDoData = [];
 
 const addLocalStorage = function () {
     let data = JSON.parse(localStorage.getItem('text'));
+    
     if (!data) {
         data = [];
     }
@@ -18,38 +16,39 @@ const addLocalStorage = function () {
 }
 toDoData = addLocalStorage();
 
+
 const render = function () {
   todoList.innerHTML = '';
   todoCompleted.innerHTML = '';
-  
-    
+   
     toDoData.forEach(function (item, index) {
     
-            const li = document.createElement('li');
-            li.classList.add('todo-item');
-            li.dataset.index = index;
-            li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.dataset.index = index;
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
                   '<div class="todo-buttons">' +
                             '<button class="todo-remove"></button>' +
                             '<button class="todo-complete"></button>' +
                           '</div>';
-            if (item.completed) {
+        if (item.completed) {
               todoCompleted.append(li);
-            } else {
+        } else {
               todoList.append(li);
-            }
+        }
              
-            li.querySelector('.todo-complete').addEventListener('click', function (){
-              item.completed = !item.completed;
-              render();
-            });
+        li.querySelector('.todo-complete').addEventListener('click', function (){
+            item.completed = !item.completed;
+            localStorage.setItem('text', JSON.stringify(toDoData));
+            render();
+            
+        });
             
             
-            li.querySelector('.todo-remove').addEventListener('click', function (){
-                
-                toDoData.splice(index, 1);
-                render();
-              
+        li.querySelector('.todo-remove').addEventListener('click', function (){
+            toDoData.splice(index, 1);
+            localStorage.setItem('text', JSON.stringify(toDoData));
+            render();
             });
              
           });
@@ -61,18 +60,16 @@ todoControl.addEventListener('submit', function (event) {
   const newToDo = {
     text: headerInput.value,
     completed: false
-  }
-  if (!headerInput.value.trim() == '') {
+  };
+  if (headerInput.value.trim() != '') {
     toDoData.push(newToDo);
     localStorage.setItem('text', JSON.stringify(toDoData));
     headerInput.value = '';
-    
     render();
-    
   } else {
     alert('Заполните поле');
   }
   
-})
-render()
+});
+render();
 
